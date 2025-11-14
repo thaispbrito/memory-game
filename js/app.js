b3d5c226b751fdacd13236360d603b2736e1d524
Thais Palma de Brito <britop.tech@gmail.com>
2025-11-13 20:25:55 -0800

    finishes project planning

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// PSEUDOCODE (Initial Project Planning) - Concentration (Memory Game)
+
+//1. First, define the variables that are required for tracking the state of the game, 
+// such as a variable that tracks the number of tries and another one that tracks the numbers of pairs, among others.
+
+//2. Think about how you want to define the number of pairs. It could be predefined by the game, set by the user or 
+// incremented by different levels of the game.
+
+//3. Make sure to store the cached element references.
+
+//4. The state of the game should be initialized when the page loads.
+
+//5. A function should be called to render the state of the game, and that state should be rendered to the user.
+
+//6. Define any required constants for the game.
+
+//7. Create all the necessary functions, including one to handle a player clicking a pair element (e.g., card).
+// This `handleClick` function should call other functions defined in the global scope.
+
+//8. Create a reset functionality, so that the game could restart once triggered by a click from the user.
+
+//9. Add event listeners to handle user interactions, at least for the pair elements and for the reset button.
+
+//10. Define how the player loses the game. It could be after a certain amount of time or a certain number of wrong guesses.
+
+//11. Define a theme for you game, such as Nature/hiking related. The pairs could be represented by cards, symbols, emojis, pictures, etc.
